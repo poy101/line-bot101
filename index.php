a817612eb6b196ae96741c055a6d7bd96b2e0316
poy101 <96290067+poy101@users.noreply.github.com>
2021-12-20 19:36:16 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 ini_set("allow_url_fopen", 1);
 $json = file_get_contents("http://www.tmy.or.th/tmymobile/webservice/linebotjsp.jsp?act=lms&_phoneNumber=1234567890");
 $obj = json_decode($json);
-echo $obj->access_token;
+echo $obj->MEM_ID;
 ?>
