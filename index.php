87e74460c396b9f3301e61d0065aa92de102892d
poy101 <96290067+poy101@users.noreply.github.com>
2021-12-20 19:49:47 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
-ini_set("allow_url_fopen", 1);
-$json = file_get_contents("http://www.tmy.or.th/tmymobile/webservice/linebotjsp.jsp?act=lms&_phoneNumber=1234567890");
-$obj = json_decode($json);
-echo $obj->MEM_ID;
+$curl= curl_init();
+ curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+ curl_setopt($curl, CURLOPT_URL, "http://www.tmy.or.th/tmymobile/webservice/linebotjsp.jsp?act=lms&_phoneNumber=1234567890");
+ $res = curl_exec($curl);
+ curl_close($curl);
+ $jo = json_decode($res);
+/*ini_set("allow_url_fopen", 1);
+$json = file_get_contents("http://www.tmy.or.th/tmymobile/webservice/linebotjsp.jsp?act=lms&_phoneNumber=1234567890");*/
+//$obj = json_decode($json);
+echo $jo->MEM_ID;
 ?>
