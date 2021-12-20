eee5112b11b258bcb622fb4e21ed469504186692
poy101 <96290067+poy101@users.noreply.github.com>
2021-12-20 16:26:37 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-echo "Hello LINE BOT by POY";
+ini_set("allow_url_fopen", 1);
+$json = file_get_contents("http://192.168.1.3:8080/tmymobile/webservice/linebotjsp.jsp?act=lms&_phoneNumber=1234567890");
+$obj = json_decode($json);
+echo $obj->access_token;
 ?>
